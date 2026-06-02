7bf3b0cee2a690004029d5799f7af34467c04d5d
Laksh Prabhu Dessai <laksh.prabhudessai@gmail.com>
2026-06-03 00:21:21 +0530

    add base url in env

--- src/api/timesheet.ts
+++ src/api/timesheet.ts
@@ -2,3 +2,13 @@
 // add task
 // edit task
 // delete task
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const getTimesheets = async () => {
+  const response = await fetch(
+    `${BASE_URL}/timesheets`
+  );
+
+  return response.json();
+};
